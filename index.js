980b48c591408bf759e2b3f5c336dc135481e64d
Md. Abdur Rahman <46405817+Riad2019@users.noreply.github.com>
2021-03-28 05:43:10 +0600

    Multiple api create

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ var express = require('express')
 var app = express()
 
 app.get('/',(req,res) => {
-    res.send("I know how to open node..yay");
+    
+    const fruit = {
+        product :'mango',
+        price : 220
+    }
+
+
+    res.send(fruit);
+});
+app.get('/fruit/banana',(req,res) => {
+    res.send({product: 'kola' ,quantity: 100 ,price :1000 })
 })
 
 app.listen(3000, ()=> console.log('listening to log 3000'))
